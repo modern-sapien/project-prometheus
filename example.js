e84ecd06175cc8a6a88866f8cff12f526d5f8559
Jonathan Canales <joncanales2@gmail.com>
2020-09-29 22:00:28 -0400

    added experimental server.js

--- example.js
+++
@@ -1,8 +0,0 @@
-class shape{    
-    constructor(area, perimeter)    {
-        this.area = area;
-        this.perimeter = perimeter;
-    }
-}
-
-const rectangle = newShape(200, 60);
---
+++ server.js
@@ -0,0 +1,14 @@
+
+const http  = require("http");
+
+const PORT = 7000;
+
+const server = http.createServer(handleRequest);
+
+server.listen(PORT, function () {
+    console.log(`Listening on http://localhost:${PORT}`);
+})
+
+function handleRequest(request, response)   {
+        response.end("This is a test, this only a test.")
+}
