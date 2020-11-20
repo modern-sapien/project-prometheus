f93d41e75244d60fe4e183502548735d1f615284
Lexx <joncanales2@gmail.com>
2020-11-20 15:19:38 -0500

    exploring the Math object through a console log of available functions.

---
+++ exercises/javascript/05-Math/app.js
@@ -0,0 +1,27 @@
+// Numbers & the Math Object
+
+const num1 = 100
+const num2 = 50
+
+let val;
+
+// simple math
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+val = Math.PI;
+val = Math.E  //Eulers number
+val = Math.round(2.4)
+val = Math.ceil(2.4)
+val = Math.floor(2.4)
+val = Math.sqrt(64) //square root
+val = Math.abs(64) // absolute
+val = Math.min(1, 23, 5, 6, 2000, 1123,64) 
+val = Math.max(1, 23, 5, 6, 2000, 1123,64) 
+val = Math.random() 
+val = Math.floor(Math.random() * 20 + 1) 
+
+console.log(val)
