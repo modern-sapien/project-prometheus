fb97d13e22c3a9e9bd58ff404fc6205c493303b9
Lexx <joncanales2@gmail.com>
2020-11-20 19:48:35 -0500

    array methods worked through.

---
+++ exercises/javascript/08-arrays-methods/app.js
@@ -0,0 +1,71 @@
+// Arrays & Methods
+
+const numbers = [55, 23, 378, 51, 4, 99]
+const numbers2 = new Array(12, 12312, 4545, 123, 96)
+const fruit = [ "apple", "banana", "orange", "peach" ]
+const mixed = [ 22, "apple", false, "banana", "orange", {a:1, b:1}, new Date(), "peach" ]
+
+let val;
+
+// Get array length
+val = numbers.length
+
+// Check if is array
+val = Array.isArray(numbers)
+
+// Get single value
+val = numbers[3]
+val = numbers[0]
+
+// Insert into array
+numbers[2] = 100
+
+// Find index of value
+// val = numbers.indexOf(51)
+
+
+// Mutate Arrays
+// Add on to end
+// numbers.push(245)
+
+// // Add on to front
+// numbers.unshift(4)
+
+// // Take off from end
+// numbers.pop();
+
+// // Take off from front
+// numbers.shift()
+
+// // Splice values
+// numbers.splice(1, 3)
+
+// // Reverse
+// numbers.reverse()
+
+// // Concatenate array
+// val = numbers.indexOf(36)
+
+// Sorting arrays
+// val = fruit.sort()
+// val = numbers.sort()
+
+// // Use the "compare function"
+// val = numbers.sort(function(x,y)    {
+//     return x- y
+// })
+
+// // Reverse sort
+// val = numbers.sort(function(x,y)    {
+//     return y- x
+// })
+
+// Find 
+function under50(num)   {
+    return num < 50
+}
+
+val = numbers.find(under50)
+
+console.log(numbers)
+console.log(val)
