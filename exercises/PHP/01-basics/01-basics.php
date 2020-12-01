a70d25e73299ccf93e7b40b2a9fd648fa6c5d918
Lexx <joncanales2@gmail.com>
2020-11-30 21:30:48 -0500

    basics explaining comments/echo/print.

---
+++ exercises/PHP/01-basics/01-basics.php
@@ -0,0 +1,25 @@
+Hello also!
+<!-- php parses HTML -->
+<h1>hello</h1>
+
+<!-- If we want to code in PHP we have to PHP tags -->
+ <?php 
+    // this is a single line comment
+    
+    # also a single line comment
+
+    /*
+    multi line syntax
+    */
+    echo "hello world"  
+    ?>
+ 
+ <!-- Php can also be wrapped in HTML tags, as long as the PHP tag is closed -->
+ <h2>
+ <?php
+    print "hello there";                
+    ?>      
+</h2>
+
+<!-- echo does not return a value, whereas print returns 1 -->
+<!-- echo is faster, and is preferred -->
