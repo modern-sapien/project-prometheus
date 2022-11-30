bcde937c4ae6e492d5146f6acd93bdf85540e12e
Modern Sapien <joncanales2@gmail.com>
2022-11-29 21:56:34 -0500

    textContent() and more use of awaits & expects

--- exercises/testAutomation/playwrightCourse/playwright.config.js
+++ exercises/testAutomation/playwrightCourse/playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { devices } = require('@playwright/test');
+const { devices } = require("@playwright/test");
 
 /**
  * Read environment variables from file.
@@ -7,13 +7,12 @@ const { devices } = require('@playwright/test');
  */
 // require('dotenv').config();
 
-
 /**
  * @see https://playwright.dev/docs/test-configuration
  * @type {import('@playwright/test').PlaywrightTestConfig}
  */
 const config = {
-  testDir: './tests',
+  testDir: "./tests",
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -21,7 +20,7 @@ const config = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 5000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -32,38 +31,40 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    browserName: "firefox",
+    headless: true,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: "chromium",
       use: {
-        ...devices['Desktop Chrome'],
+        ...devices["Desktop Chrome"],
       },
     },
 
     {
-      name: 'firefox',
+      name: "firefox",
       use: {
-        ...devices['Desktop Firefox'],
+        ...devices["Desktop Firefox"],
       },
     },
 
     {
-      name: 'webkit',
+      name: "webkit",
       use: {
-        ...devices['Desktop Safari'],
+        ...devices["Desktop Safari"],
       },
     },
 
--- exercises/testAutomation/playwrightCourse/tests/UIBasicTest.spec.js
+++ exercises/testAutomation/playwrightCourse/tests/UIBasicTest.spec.js
@@ -1,4 +1,4 @@
-const { test } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
 // browser information is passed from config file
 test("browser context playwright test", async function ({ browser }) {
@@ -7,8 +7,34 @@ test("browser context playwright test", async function ({ browser }) {
   const page = await context.newPage();
 
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  // targeting id selector & downloaded selectorshub
+  await page.locator("#username").type("rahulshetty");
+  await page.locator("[type='password']").type("learning");
+  await page.locator("#signInBtn").click();
 });
 
-test("page playwright test", async function ({ page }) {
+// login error message
+test.only("login error message appears on failure", async function ({ browser }) {
+  // creating a new or fresh context when choosing a particular browser if not delcaring
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.locator("[type='password']").type("learning");
+  await page.locator("#signInBtn").click();
+  // auto waits until matching element is displayed
+  // textContent() calls the text of the element to be displayed
+  console.log(await page.locator("[style*='block']").textContent());
+  await expect(page.locator("[style*='block']")).toContainText("Empty username/password.");
+});
+
+
+
+// using default parameters
+test("page playwright test", async function ({ page }) {
+  await page.goto("https://google.com");
+  // get title of page
+  console.log("this is a log of the title: " + (await page.title()));
+
+  await expect(page).toHaveTitle("Google");
 });
